4c6a0c6115461a96aa5b68978a39ea8bf48a944c
AhadSaleem7 <148374874+AhadSaleem7@users.noreply.github.com>
2024-06-15 11:39:53 +0530

    added bestjs.js

---
+++ bestjs.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#element', {
+    strings: ["Coder", "App Developer", "Graphic Designer ", "UI/UX Designer", "Game Developer", "Video Editor", "Web Developer "],
+    typeSpeed: 50,
+});
+
+
